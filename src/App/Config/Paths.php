d7d9438908cca6a9e3d3855601a3f77f993380b9
emmanuelc <camuschinox@gmail.com>
2024-01-09 15:13:11 -0300

    Added new path to ApiData folder in PATH config file

--- src/App/Config/Paths.php
+++ src/App/Config/Paths.php
@@ -6,6 +6,6 @@ class Paths
 {
     public const SOURCE_PATH = "./../";
     public const VIEWS_PATH = __DIR__ . "/../../Framework/View";
-
+    public const API_DATA = __DIR__ . "/../../Framework/Utils/ApiData";
     public const ENV_PATH = __DIR__ . "/../Config";
 }
